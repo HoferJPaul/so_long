d26f60de76fad00b348197cd626d5dc514ddd052
Paul Hofer <phofer@c2r4s1.42prague.com>
2025-09-12 18:55:41 +0200

    added all

---
+++ src/render2.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phofer <phofer@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 15:08:16 by phofer            #+#    #+#             */
+/*   Updated: 2025/09/12 16:18:02 by phofer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+
+static int	load_player_images(t_data *data)
+{
+	data->player_front.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/player_front.xpm", &data->player_front.width,
+			&data->player_front.height);
+	data->player_back.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/player_back.xpm", &data->player_back.width,
+			&data->player_back.height);
+	data->player_side_r.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/player_side_r.xpm", &data->player_side_r.width,
+			&data->player_side_r.height);
+	data->player_side_l.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/player_side_l.xpm", &data->player_side_l.width,
+			&data->player_side_l.height);
+	if (!data->player_front.img || !data->player_back.img
+		|| !data->player_side_r.img || !data->player_side_l.img)
+		return (1);
+	return (0);
+}
+
+static int	load_enemy_images(t_data *data)
+{
+	data->enemy_front.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/enemy_front.xpm", &data->enemy_front.width,
+			&data->enemy_front.height);
+	data->enemy_back.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/enemy_back.xpm", &data->enemy_back.width,
+			&data->enemy_back.height);
+	data->enemy_side_r.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/enemy_side_r.xpm", &data->enemy_side_r.width,
+			&data->enemy_side_r.height);
+	data->enemy_side_l.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/enemy_side_l.xpm", &data->enemy_side_l.width,
+			&data->enemy_side_l.height);
+	if (!data->enemy_front.img || !data->enemy_back.img
+		|| !data->enemy_side_r.img || !data->enemy_side_l.img)
+		return (1);
+	return (0);
+}
+
+static int	load_basic_images(t_data *data)
+{
+	data->wall.img = mlx_xpm_file_to_image(data->mlx, "textures/wall.xpm",
+			&data->wall.width, &data->wall.height);
+	data->floor.img = mlx_xpm_file_to_image(data->mlx, "textures/bg.xpm",
+			&data->floor.width, &data->floor.height);
+	data->collectible.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/item.xpm", &data->collectible.width,
+			&data->collectible.height);
+	data->exit_closed.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/exit_closed.xpm", &data->exit_closed.width,
+			&data->exit_closed.height);
+	data->exit_open.img = mlx_xpm_file_to_image(data->mlx,
+			"textures/exit_open.xpm", &data->exit_open.width,
+			&data->exit_open.height);
+	data->win_img.img = mlx_xpm_file_to_image(data->mlx, "textures/win.xpm",
+			&data->win_img.width, &data->win_img.height);
+	data->lose_img.img = mlx_xpm_file_to_image(data->mlx, "textures/lose.xpm",
+			&data->lose_img.width, &data->lose_img.height);
+	if (!data->wall.img || !data->floor.img || !data->collectible.img
+		|| !data->exit_closed.img || !data->exit_open.img
+		|| !data->win_img.img || !data->lose_img.img)
+		return (1);
+	return (0);
+}
+
+int	load_img(t_data *data)
+{
+	if (load_basic_images(data) || load_player_images(data)
+		|| load_enemy_images(data))
+	{
+		ft_printf("Error\nFailed to load one or more textures\n");
+		return (1);
+	}
+	data->text_bg.img = mlx_new_image(data->mlx, 250, 25);
+	data->current_player = &data->player_front;
+	data->current_enemy = &data->enemy_front;
+	data->current_exit = &data->exit_closed;
+	return (0);
+}
+
+void	render(t_data *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (data->map[i])
+	{
+		j = 0;
+		while (data->map[i][j])
+		{
+			draw_tile(data, j * TILE_SIZE, i * TILE_SIZE, data->map[i][j]);
+			++j;
+		}
+		++i;
+	}
+	draw_text(data);
+}
---
+++ src/so_long.h
@@ -0,0 +1,184 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phofer <phofer@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/10 14:29:06 by phofer            #+#    #+#             */
+/*   Updated: 2025/09/12 18:04:37 by phofer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# define TILE_SIZE 50
+# define MAX_MSG_LEN 64
+# define ENEMY_SPEED 500
+
+# include "mlx.h"
+# include "libft.h"
+# include "get_next_line.h"
+# include "ft_printf.h"
+# include <fcntl.h>
+# include <stdlib.h>
+
+/* --- Enemy support ------------------------------------------------------- */
+
+# define TILE_WALL       '1'
+# define TILE_FLOOR      '0'
+# define TILE_PLAYER     'P'
+# define TILE_EXIT       'E'
+# define TILE_COLLECT    'C'
+# define TILE_ENEMY      'X'
+
+typedef struct s_data t_data;
+
+typedef struct s_enemy
+{
+	int x;
+	int y;
+	int dir;     /* 0=up,1=right,2=down,3=left (optional) */
+}	t_enemy;
+
+typedef struct s_enemies
+{
+	t_enemy	*arr;
+	int		count;
+	long	last_ms;      /* last update time in ms */
+	int		interval_ms;  /* how often to move (e.g., 220) */
+}	t_enemies;
+
+typedef struct s_img
+{
+	void	*img;
+	int		width;
+	int		height;
+	int		bits_per_pixel;
+}	t_img;
+
+typedef struct s_player
+{
+	int x;
+	int y;
+	int moves;
+}	t_player;
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*mlx_win;
+	void	*win_screen;
+	void	*lose_screen;
+	char	**map;
+	char	**map_cpy;
+	char	*map_file;
+	char	last_message[MAX_MSG_LEN];
+	char	message[MAX_MSG_LEN];
+	t_img	text_bg;
+	t_img	win_img;
+	t_img	lose_img;
+	t_img	wall;
+	t_img	floor;
+	t_img	collectible;
+	t_img	exit_open;
+	t_img	exit_closed;
+	t_img	player_front;
+	t_img	player_back;
+	t_img	player_side_r;
+	t_img	player_side_l;
+	t_img	enemy_front;
+	t_img	enemy_back;
+	t_img	enemy_side_r;
+	t_img	enemy_side_l;
+	t_img	*current_enemy;
+	t_img	*current_player;
+	t_img	*current_exit;
+	int		size_x;
+	int		size_y;
+	int		p_x;
+	int		p_y;
+	int		player_x;
+	int		player_y;
+	int		moves;
+	int		items_total;
+	int		items_left;
+	int		game_over;
+	int		collectibles_found;
+	int		exit_found;
+	t_player	player;
+	t_enemies	enemies;
+}	t_data;
+
+/* main.c */
+int		count_lines(int fd);
+int		validate_map(int argc, char **argv);
+
+/* game.c */
+int		exit_on_key(int keycode, t_data *data);
+int		close_window(t_data *data);
+int		find_player(t_data *data);
+
+/* game2.c */
+void	show_win_screen(t_data *data);
+void	show_lose_screen(t_data *data);
+int		push_back(t_data *data, int keycode);
+
+/* input.c */
+int		handle_keypress(int keycode, t_data *data);
+int		validate_move(t_data *data, int new_x, int new_y);
+int		handle_collectible(t_data *data, int new_x, int new_y);
+
+/* input2.c */
+int		handle_exit(t_data *data, int keycode);
+void	update_player_position(t_data *data, int new_x, int new_y);
+int		handle_enemy(t_data *data);
+
+/* render.c */
+int		set_window_size(t_data *data, t_img *img);
+void	text_window(t_data *data);
+
+/* render2.c */
+int		load_img(t_data *data);
+void	render(t_data *data);
+
+/* render3.c */
+void	draw_tile(t_data *data, int x, int y, char c);
+
+/* draw_text.c */
+void	draw_text(t_data *data);
+void	set_message(t_data *data, const char *msg);
+void	set_message_with_number(t_data *data, const char *prefix, int number, const char *suffix);
+void	clear_message(t_data *data);
+
+/* map_parser.c */
+int		is_rectangular(t_data *data);
+int		is_surrounded(t_data *data);
+
+/* map_parser2.c */
+int		validate_characters(t_data *data);
+int		check_char(t_data *data);
+int		count_lines(int fd);
+void	remove_newline(char *line);
+
+/* flood_fill.c */
+int		validate_path(t_data *data);
+void	flood_fill_recursive(t_data *data, int x, int y);
+
+/*food_fill2.c*/
+void	free_map_copy(char **map_copy);
+int		check_exit(t_data *data);
+
+/* cleanup.c*/
+void	free_map(t_data *data);
+void	free_textures(t_data *data);
+void	close_windows(t_data *data);
+void	cleanup_game(t_data *data);
+
+/* enemy API */
+int  enemies_init(struct s_data *d);         /* scan map for 'X' and alloc */
+void enemies_free(struct s_data *d);
+int  enemies_tick(struct s_data *d);         /* hook for mlx_loop_hook     */
+
+#endif
